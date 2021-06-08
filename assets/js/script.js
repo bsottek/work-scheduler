fe24174cad59f49e7afa05f6843c8583b040836d
bsottek <bsottek@gmail.com>
2021-06-07 20:54:14 -0500

    color-coding for past, present, and future hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,32 @@
+var hours = $('.time-block')
+var desc = $('.description')
 var today = moment().format('LLLL');
-console.log(today);
 $('#currentDay').html(today);
 
 
-setInterval(function(){
+var timeCheck = function(){
     var today = moment().format('LLLL');
-    console.log(today);
     $('#currentDay').html(today);
-}
-, 60000);
 
+    for(var i = 0; i < hours.length; i++)   {
+        var hoursInt = []
+        hoursInt[i] = parseInt(hours[i].textContent.substring(0,2));
+
+        if(hoursInt[i] < parseInt(moment().format('HH'))){
+            desc[i].classList.add('past');
+        } else if (hoursInt[i] > parseInt(moment().format('HH'))){
+            desc[i].classList.add('future');
+        } else {
+            desc[i].classList.add('present');
+        }
+    }
+};
+
+setInterval(timeCheck(),60000);
+
+
+timeCheck();
+
+
+// the time blocks are being interpereted as now rather than their respective times.
+// something to do with moment()
